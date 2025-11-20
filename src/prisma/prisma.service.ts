2a9d406dc11ab11eadc2d69c4379b118d2764192
Juan Pareja <pareja.juan@hotmail.com>
2025-11-20 11:05:18 -0400

    feat: add prisma module

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from '@generated/prisma/client';
+
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
