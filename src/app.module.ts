9f3b008f29918d075d6bd8f11db949d847460eb6
Juan Pareja <pareja.juan@hotmail.com>
2025-11-20 10:48:00 -0400

    feat: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
+import { AppController } from '@src/app.controller';
+import { AppService } from '@src/app.service';
+
+import { SecurityModule } from '@security/security.module';
+
+import { AccountModule } from '@accounts/account.module';
+
+import { ContactModule } from '@contacts/contact.module';
+
+import { FacilityModule } from '@facilities/facility.module';
+
+import { OpportunityModule } from '@opportunities/opportunity.module';
+
+import { PermissionModule } from '@permissions/permission.module';
+
+import { RoleModule } from '@roles/role.module';
+
+import { TenantModule } from '@tenants/tenant.module';
+
+import { UserModule } from '@users/user.module';
+
+import { QuoteModule } from '@quotes/quote.module';
+
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [AccountModule, ContactModule, FacilityModule, OpportunityModule, PermissionModule, QuoteModule, RoleModule, SecurityModule, TenantModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
