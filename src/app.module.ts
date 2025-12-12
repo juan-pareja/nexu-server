ec770cebcc262e2dbcf9ecf454e9db140e741388
Juan Pareja <pareja.juan@hotmail.com>
2025-12-12 17:13:05 -0400

    refactor: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { AppController } from '@src/app.controller';
 import { AppService } from '@src/app.service';
+import { PrismaModule } from '@src/prisma/prisma.module';
 
 import { SecurityModule } from '@security/security.module';
 
@@ -7,7 +8,11 @@ import { AccountModule } from '@accounts/account.module';
 
 import { ContactModule } from '@contacts/contact.module';
 
+import { CurrencyModule } from '@currencies/currency.module';
+
 import { FacilityModule } from '@facilities/facility.module';
+
+import { LocationModule } from '@locations/location.module';
 
 import { OpportunityModule } from '@opportunities/opportunity.module';
 
@@ -22,9 +27,28 @@ import { UserModule } from '@users/user.module';
 import { QuoteModule } from '@quotes/quote.module';
 
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AccountModule, ContactModule, FacilityModule, OpportunityModule, PermissionModule, QuoteModule, RoleModule, SecurityModule, TenantModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    AccountModule,
+    ContactModule,
+    CurrencyModule,
+    FacilityModule,
+    LocationModule,
+    OpportunityModule,
+    PermissionModule,
+    PrismaModule,
+    QuoteModule,
+    RoleModule,
+    SecurityModule,
+    TenantModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
